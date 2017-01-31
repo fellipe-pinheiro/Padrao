ee972c1919cfb790d88656452aa6f4a458d95645
ericnakaw <ericnakaw>
2017-01-31 19:55:44 -0200

    Bug fix: Menu responsivo não funcionava.

--- application/views/_container/container_itens.php
+++ application/views/_container/container_itens.php
@@ -38,10 +38,10 @@ foreach ($arr as $key => $value) {
 			<div class="panel panel-default">
 			    <div class="panel-body panel-nav">
 			        <nav class="navbar navbar-default navbar-static-top" role="navigation">
-			            <div class="container-fluid">
+			            <div class="container">
 			                <!-- Brand and toggle get grouped for better mobile display -->
 			                <div class="navbar-header">
-			                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-item-<?=$tabela_id?>" aria-expanded="false" aria-controls="navbar">
 			                        <span class="sr-only">Toggle navigation</span>
 			                        <span class="icon-bar"></span>
 			                        <span class="icon-bar"></span>
@@ -51,7 +51,7 @@ foreach ($arr as $key => $value) {
 			                </div>
 			                
 			                <!-- Collect the nav links, forms, and other content for toggling -->
-			                <div class="collapse navbar-collapse">
+			                <div class="collapse navbar-collapse navbar-item-<?=$tabela_id?>">
 			                    <ul class="nav navbar-nav">
 			                        <li>
 			                        	<a onclick="abrir_papel_modal('<?=$session_owner->owner?>')" data-toggle="modal" href='#md_papel'><i class="glyphicon glyphicon-file"></i> Papel</a>
