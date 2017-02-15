f572eacc9933fd1eaf198b7df432b603180df2b8
ericnakaw <ericnakaw>
2017-02-15 21:58:44 -0200

    Melhoria na função que retorna a data para ser gravada no banco de dados aceitando os seguintes formatos: [dd/mm/yyyy] [ yyyy/mm/dd] [dd-mm-yyyy] [ yyyy-mm-dd]

--- application/helpers/formatar_helper.php
+++ application/helpers/formatar_helper.php
@@ -1,8 +1,22 @@
 <?php 
 function date_to_db($data=''){
 	if (!empty($data)) {
-		list($dia, $mes, $ano) = explode('/', $data);
-        return $data = $ano . '-' . $mes . '-' . $dia;
+		if (strpos($data, '/') !== false) {// Aceita os formatos: dd/mm/yyyy - yyyy/mm/dd
+			list($dia, $mes, $ano) = explode('/', $data);
+			if(strlen($dia) === 2){
+	        	return $data = $ano . '-' . $mes . '-' . $dia;
+			}else if(strlen($dia) === 4){
+				list($ano, $mes, $dia) = explode('/', $data);
+				return $data = $ano . '-' . $mes . '-' . $dia;
+			}
+		}else if(strpos($data, '-') !== false){// Aceita os formatos: dd-mm-yyyy - yyyy-mm-dd
+			list($dia, $mes, $ano) = explode('-', $data);
+			if(strlen($dia) === 2){
+	        	return $data = $ano . '-' . $mes . '-' . $dia;
+			}else if(strlen($dia) === 4){
+				return $data;
+			}
+		}
 	}
 	return "";
 }
