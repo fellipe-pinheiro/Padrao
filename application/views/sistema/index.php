a80e352ddd924e7cdb66ef07f740f1a3a2f6c7b8
ericnakaw <ericnakaw>
2017-02-24 20:34:08 -0300

    Ajustes na view do sistema/index

--- application/views/sistema/index.php
+++ application/views/sistema/index.php
@@ -1,33 +1,59 @@
-<form action="" id="form_configuracao_sistema" method="POST" role="form">
-	<fieldset>
-		<legend>Painel de Configuração do Sistema</legend>
-		<div class="col-sm-6">
-			<div class="form-group">
-				<label for="parcelamento_maximo">Parcelamento maximo</label>
-				<input type="number" class="form-control" name="parcelamento_maximo" id="parcelamento_maximo" placeholder="12" value="<?=$dados["parcelamento_maximo"]?>">
-				<span class="help-block"></span>
-			</div>
-			<div class="form-group">
-				<label for="valor_minimo_parcelamento">Valor minimo para parcelamento</label>
-				<input type="number" class="form-control" name="valor_minimo_parcelamento" id="valor_minimo_parcelamento" placeholder="100,00" value="<?=$dados['valor_minimo_parcelamento']?>">
-				<span class="help-block"></span>
-			</div>
-		</div>
-		<div class="col-sm-6">
-			<div class="form-group">
-				<label for="prazo_validade_orcamento">Prazo de validade do orçamento</label>
-				<input type="number" class="form-control" name="prazo_validade_orcamento" id="prazo_validade_orcamento" placeholder="30" value="<?=$dados['prazo_validade_orcamento']?>">
-				<span class="help-block"></span>
-			</div>
-		</div>
-	</fieldset>
-	<div class="col-sm-12">
-		<button type="submit" class="btn btn-primary">Submit</button>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">Configuração do sistema</h3>
 	</div>
-</form>
+	<div class="panel-body">
+		<form action="" id="form_configuracao_sistema" method="POST" role="form">
+			<fieldset>
+				<div class="col-sm-6">
+					<div class="form-group">
+						<label for="parcelamento_maximo" class="control-label col-sm-6">Parcelamento máximo</label>
+						<div class="col-sm-6">
+							<div class="input-group">
+								<input type="number" class="form-control" name="parcelamento_maximo" id="parcelamento_maximo" placeholder="Ex: 12" value="<?=$dados["parcelamento_maximo"]?>">
+								<div class="input-group-addon">vezes</div>
+							</div>
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="valor_minimo_parcelamento" class="control-label col-sm-6">Valor mínimo parcelamento</label>
+						<div class="col-sm-6">
+							<div class="input-group">
+								<input type="number" class="form-control" name="valor_minimo_parcelamento" id="valor_minimo_parcelamento" placeholder="Ex: 100,00" value="<?=$dados['valor_minimo_parcelamento']?>">
+								<div class="input-group-addon">reais</div>
+							</div>
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="prazo_validade_orcamento" class="control-label col-sm-6">Validade do orçamento</label>
+						<div class="col-sm-6">
+							<div class="input-group">
+								<input type="number" class="form-control" name="prazo_validade_orcamento" id="prazo_validade_orcamento" placeholder="Ex: 7" value="<?=$dados['prazo_validade_orcamento']?>">
+								<div class="input-group-addon">dias</div>
+							</div>
+							<span class="help-block"></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6"></div>
+				<div class="col-sm-12">
+					<div class="col-sm-6">
+						<button type="submit" class="btn btn-success btnSubmit">Salvar</button>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
+
 <script type="text/javascript">
 	$(document).ready(function() {
 		$("#form_configuracao_sistema").submit(function(event) {
+			event.preventDefault();
+			disable_button_salvar();
+			reset_errors();
 			$.ajax({
 				url: '<?=base_url("sistema/ajax_update")?>',
 				type: 'post',
@@ -55,9 +81,8 @@
 			})
 			.always(function() {
 				console.log("complete");
+				enable_button_salvar();
 			});
-
-			event.preventDefault();
 		});
 		
 		
